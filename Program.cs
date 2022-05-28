299d0fbcd8e4819b773a8b81fe1080641e51133e
“00Amit0” <amit836961@gmail.com>
2022-05-28 23:05:02 +0530

    Welcome to Logical Programs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            /*Console.WriteLine("Welcome to Logical Programs");
+            PerfectNumber.FindPerfectNumber();
+            Reverse.ReverseTheNumber();
+            Fibonacci.CheckFibonacci();
+            PrimeNumber.FindPrimeNumber();
+            Coupon.GenerateCoupon();*/
+            Temperature.PrintTemperature();
+            MonthlyPayment.Payment();
+            VendingMachine.Notes();
+            ToBinary.Binary();
+            Stop.watch();
+            SquareRoot.Sqrt();
+            SwapNibbles.Binaryy();
+            DayOfWeek.FindDay();
+        }
+    }
+}
