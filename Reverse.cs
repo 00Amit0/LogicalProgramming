b2e0f889c62bf644ef849e9584d3f870935f3158
“00Amit0” <amit836961@gmail.com>
2022-05-28 23:17:35 +0530

    Reversing numbers

---
+++ Reverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Reverse
+    {
+        public static void ReverseTheNumber()
+        {
+            int n, reverse = 0, rem = 0;
+            Console.WriteLine("Enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Reversed Number :" + reverse);
+        }
+    }
+}
