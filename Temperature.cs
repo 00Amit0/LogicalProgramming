e9f5448aa9644c784791f1fa3c7c8c6c94cfca34
“00Amit0” <amit836961@gmail.com>
2022-05-28 23:32:00 +0530

    Is it hot or Cold ?

---
+++ Temperature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Temperature
+    {
+        public static void PrintTemperature()
+        {
+            double f, c;
+            Console.WriteLine("Enter the value of Celsius:");
+            c = Convert.ToDouble(Console.ReadLine());
+            f = c * 9 / 5 + 32;
+            Console.WriteLine(c + "°C in Fahrenheit is: " + f + "°F");
+            Console.WriteLine("Enter the value of Fahrenheit:");
+            f = Convert.ToDouble(Console.ReadLine());
+            c = (f - 32) * 5 / 9;
+            Console.WriteLine(f + "°F in Celsius is: " + c + "°C");
+            Console.ReadLine();
+        }
+    }
+}
